2c5a6db2a754776f4ad5ab138a05392f237ba273
Evan Epperson <evanepperson@pop-os.localdomain>
2021-03-04 09:49:23 -0600

    saving

--- postgres.js
+++ postgres.js
@@ -1,16 +1,32 @@
-const Client = require('pg').Client
+// const Client = require('pg').Client
+
+// // const client = new Client({
+// //     connectionString: process.env.DATABASE_URL || 'postgresql://localhost:5432/contacts',
+// // })
 
 // const client = new Client({
-//     connectionString: process.env.DATABASE_URL || 'postgresql://localhost:5432/contacts',
-// })
+//   user: "evanepperson",
+//   host: "localhost",
+//   database: process.env.DATABASE_URL || "contacts",
+//   password: "password",
+//   port: 5432,
+// //   ssl: true,
+// });
 
-const client = new Client({
-  user: "evanepperson",
-  host: "localhost",
-  database: process.env.DATABASE_URL || "contacts",
-  password: "password",
-  port: 5432,
-  ssl: true,
-});
+// module.exports = client;
+
+
+cont Client = require('pg').Client
+
+const dbConfig = {
+    connectionString: 'postgresql://localhost:5432/contacts' 
+}
+
+if(process.env.DATABASE_URL){
+    dbConfig.ssl ={rejectUnauthorized: false}
+    dbConfig.connectionString = process.env.DATABASE_URL
+}
+
+const client = new Client(dbConfig)
 
 module.exports = client;
